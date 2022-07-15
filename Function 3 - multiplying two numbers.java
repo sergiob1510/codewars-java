31d61c2c67d9ee231989878d37af33ed18f576cc
Sergio Bianco <sergio.bianco@live.com>
2022-07-15 12:26:36 -0300

    Update and rename Function 3 - multiplying two numbers to Function 3 - multiplying two numbers.java

---
+++ Function 3 - multiplying two numbers.java
@@ -0,0 +1,10 @@
+/**
+* <h2>Assignment</h2>
+* Implement a function which multiplies two numbers.
+* <h2>Solution</h2>
+*/
+public class Kata {
+  public final int multiply(int a, int b) {
+    return a * b;
+  }
+}
