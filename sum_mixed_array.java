691d56a9e6b28907382576f46c74335c53703c2a
Sergio Bianco <sergio.bianco@live.com>
2022-07-27 18:40:05 -0300

    Update sum_mixed_array.java

--- sum_mixed_array.java
+++ sum_mixed_array.java
@@ -1,4 +1,6 @@
-/* <h2> Sum Mixed Array </h2>
+/** <h2> Sum Mixed Array </h2>
+*
+* Description:
 *
 * Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers. Return your answer as a number.
 *
